79c9da7b5b1a8eb3e0d925a2112fdf1f2ae6443a
Shaunak Pagnis <shaunak.pagnis@gmail.com>
2021-05-24 01:43:48 -0400

    Introduces Dark mode
    - Auto selects mode based on user's OS preference
    - Provides a button to toggle mode
    - Fixes Solarized colors to handle accessibility color contrasts in dark mode
    - Removes dead css code

--- scripts.js
+++ scripts.js
@@ -6,14 +6,36 @@ function expandGroups(event) {
   Array.prototype.forEach.call(els, function(el) {
     el.addEventListener(event, function () {
       el.parentElement.classList.toggle('expand');
-      if(el.classList.contains('flash')) {
+      if (el.classList.contains('flash')) {
         el.classList.remove('flash');
       }
     }, false);
   });
 }
+
+function toggleDisplayMode(isDarkMode) {
+  var toggleBtn = document.getElementsByClassName('toggle-mode')[0];
+  if (isDarkMode) {
+    document.body.classList.add('dark');
+    document.body.classList.remove('light');
+    toggleBtn.textContent = '[Light]'
+  } else {
+    document.body.classList.add('light');
+    document.body.classList.remove('dark');
+    toggleBtn.textContent = '[Dark]'
+  }
+}
+
 (function () {
   window.addEventListener('load', function() {
     expandGroups('click');
+
+    var prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+    toggleDisplayMode(prefersDarkScheme.matches);
+
+    var toggleBtn = document.getElementsByClassName('toggle-mode')[0];
+    toggleBtn.addEventListener('click', function () {
+      toggleDisplayMode(document.body.classList.contains('light'));
+    })
   }, false);
 })();
